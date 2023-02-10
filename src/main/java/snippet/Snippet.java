89870cd4df1be0d3219877def2ea66d780d7b0b4
João Henrique <joaohenriquerdl@gmail.com>
2023-02-10 17:40:34 -0300

    first commit

---
+++ src/main/java/snippet/Snippet.java
@@ -0,0 +1,6 @@
+package snippet;
+
+public class Snippet 
+	C:\Users\João Henrique\AppData\Local\Temp\Rar$EXa23252.18026\sts-4.17.2.RELEASE
+}
+
---
+++ src/test/java/com/Estudos/EstudosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
